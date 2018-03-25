8a8c4469f5456ba46974132bac3bb19d78eb4aec
Shashank <saishashank.rasamalla@cognizant.com>
2018-03-25 11:22:56 -0500

    fixing react sparkline issue, upgrading jquery and some long overdue code clean up

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,19 +1,25 @@
 import _ from 'lodash';
-import React from 'react';
-import {Sparklines, SparklinesLine, SparklinesReferenceLine} from 'react-sparklines';
+import React, { Component } from 'react';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
-function average(data){
-  return _.round(_.sum(data)/data.length);
+function average(data) {
+	return _.round(_.sum(data) / data.length);
+}
+class Chart extends Component {
+	render() {
+		const { data, units, color } = this.props;
+		return (
+			<div>
+				<Sparklines height={120} width={180} data={data}>
+					<SparklinesLine color={color} />
+					<SparklinesReferenceLine type="avg" />
+				</Sparklines>
+				<div>
+					{average(data)} {units}
+				</div>
+			</div>
+		);
+	}
 }
 
-export default (props) =>{
-return(
-  <div>
-  <Sparklines height={120} width={180} data ={props.data}>
-  <SparklinesLine color={props.color} />
-  <SparklinesReferenceLine type="avg" />
-  </Sparklines>
-  <div>{average(props.data)} {props.units}</div>
-  </div>
-);
-}
+export default Chart;
--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { GoogleMapLoader, GoogleMap} from 'react-google-maps';
+import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
 
-export default (props) =>{
-  return(
-<GoogleMapLoader
-containerElement={ <div style={{height:'100%'}} /> }
-googleMapElement={
-  <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}} />
-}
-/>
-  );
-}
+export default props => {
+	return (
+		<GoogleMapLoader
+			containerElement={<div style={{ height: '100%' }} />}
+			googleMapElement={<GoogleMap defaultZoom={12} defaultCenter={{ lat: props.lat, lng: props.lon }} />}
+		/>
+	);
+};
